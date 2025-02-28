11fb8b8287529eefe879587075f0adef9b20016b
Gago <egagik69@gmail.com>
2025-02-28 14:45:42 +0400

    create API service and get data from JSON files

--- src/app/components/candidates/candidates.component.ts
+++ src/app/components/candidates/candidates.component.ts
@@ -1,11 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, inject, Signal } from '@angular/core';
+import { CandidateInterface } from '../../types/candidate.interface';
+import { TaskInterface } from '../../types/task.interface';
+import { ApiService } from '../../api.service';
+import { toSignal } from '@angular/core/rxjs-interop';
 
 @Component({
+
   selector: 'app-candidates',
   imports: [],
   templateUrl: './candidates.component.html',
   styleUrl: './candidates.component.scss'
 })
 export class CandidatesComponent {
+ 
+  private apiService = inject(ApiService)
+  public candidates:Signal<CandidateInterface[] |undefined> = toSignal(this.apiService.getCandidates());
+  public task:Signal<TaskInterface|undefined> = toSignal(this.apiService.getTask())
+
+  constructor(){}
 
 }
--- src/app/types/candidate.interface.ts
+++ src/app/types/candidate.interface.ts
@@ -1,4 +1,4 @@
-export interface Candidate{
+export interface CandidateInterface{
     nameSurname:string,
     startDate:Date,
     endDate:Date,
