91f987670d09dfe7be40bafc8cd8c40b454f0bc1
Gago <egagik69@gmail.com>
2025-02-28 12:37:55 +0400

    install ngx-charts and  moment, create interface

---
+++ src/app/types/task.interface.ts
@@ -0,0 +1,7 @@
+export interface Task{
+    name:string,
+    startDate:string,
+    endDate:string,
+    primarySkill:string,
+    secondarySkill:string,
+}
---
+++ src/app/types/worker.interface.ts
@@ -0,0 +1,7 @@
+export interface Worker{
+    nameSurname:string,
+    startDate:string,
+    endDate:string,
+    primarySkill:string,
+    secondarySkill:string,
+}
